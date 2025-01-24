4b0305999bafe3b0a1ed18394bef0aa901abb5d2
Leonardo Eljadue <leo.eljadues@gmail.com>
2025-01-24 13:20:49 -0500

    Versión final del proyecto, se agrego el README y se desarrollo el código funcional en JS

---
+++ README.md
@@ -0,0 +1,22 @@
+# Juego de Amigo Secreto 🎉
+
+Este es un juego sencillo de Amigo Secreto desarrollado para agregar nombres a una lista y sortear un amigo secreto de forma aleatoria.
+
+## Funcionalidades 🛠️
+
+1. **Cuadro de texto para ingresar nombres**: Permite escribir el nombre de los amigos que participarán en el sorteo.
+2. **Botón "Añadir"**:
+   - Agrega el nombre ingresado a una lista.
+   - Si el cuadro de texto está vacío, muestra una alerta: `"Por favor, inserte un nombre."`.
+   - Cada nombre agregado se muestra en pantalla.
+3. **Botón "Sortear amigo"**:
+   - Si la lista está vacía, muestra una alerta: `"Su lista de amigos esta vacía."`.
+   - Si la lista no esta vacía realiza el sorteo de un amigo de forma aleatoria y muestra el mensaje: `"El amigo secreto sorteado es: Nombre Aleatorio"`.
+
+## Instrucciones de Uso 📋
+
+1. Ingresa un nombre en el cuadro de texto.
+2. Haz clic en el botón **"Añadir"** para agregar el nombre a la lista.
+3. Repite el paso anterior para todos los participantes.
+4. Una vez que todos los nombres estén en la lista, haz clic en **"Sortear amigo"**.
+   - Se mostrará el nombre del amigo secreto sorteado.
--- app.js
+++ app.js
@@ -1,1 +1,53 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Crea la lista de amigos
+let amigos = [];
+
+/*Función que permite ingresar la lista de amigos, valida si un valor es vació, si lo es, muestra el mensaje "Por favor inserte un nombre"
+si el valor es validó lo agrega con .push() y limpia el campo de texto*/
+function agregarAmigo(){
+
+    let amigosUsuario = document.getElementById('amigo').value;
+
+    if(amigos.length>=0 && amigosUsuario == ''){
+        alert("Por favor, inserte un nombre.")
+    }else{
+        amigos.push(amigosUsuario);
+        console.log(amigos);
+        mostrarNombres(amigos);
+        limpiarCaja();
+    } 
+}
+//Función que permite limpiar el cuadro de texto, cada vez que añada un nuevo nombre a la lista.
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+//Función que permite mostrar los nombres conforme se van agregando
+function mostrarNombres(listaAmigos){
+
+    let elementoHTML = document.getElementById('listaAmigos');
+    elementoHTML.innerHTML = "";
+
+    for(posicion=0;posicion<listaAmigos.length;posicion++){
+        let li = document.createElement('li');
+        li.textContent = listaAmigos[posicion];
+        elementoHTML.appendChild(li);
+    }
+}
+
+//Función que permite mostrar el amigo secreto de manera aleatoria, limpiando en pantalla al lista de amigos previamente ingresada.
+
+function sortearAmigo(){
+
+    let amigoSecreto = Math.floor(Math.random()*amigos.length);
+    let elementoAmigoHTML = document.getElementById('resultado');
+
+    if(amigos.length==0){
+        alert("Su lista de amigos esta vacía");
+    }else{
+        let elementoHTML = document.getElementById('listaAmigos');
+        elementoHTML.innerHTML = "";
+        return(elementoAmigoHTML.innerHTML = (`El amigo secreto sorteado es: ${amigos[amigoSecreto]}`));      
+    }
+}
+
